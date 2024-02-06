7cf1b06b79db48605ed50588b723b7f9b9deddab
shubhamhushe1 <shubhamhushe1@gmail.com>
2024-02-06 13:45:34 +0530

    HTTP Client Post Method for Company

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AdditionComponent } from './addition/addition.component';
 import { ArrayExComponent } from './array-ex/array-ex.component';
 import { PipesExComponent } from './pipes-ex/pipes-ex.component';
 import { DynamicroutingComponent } from './dynamicrouting/dynamicrouting.component';
+import { CompanyComponent } from './company/company.component';
 
 export const routes: Routes = [
 
@@ -13,5 +14,7 @@ export const routes: Routes = [
 
 {path: 'pipes-ex', component:PipesExComponent},
 
-{ path : 'dynamicrouting', component:DynamicroutingComponent}
+{ path : 'dynamicrouting', component:DynamicroutingComponent},
+
+{ path : 'company', component:CompanyComponent}
 ];
---
+++ src/app/company/company.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Company, CompanyService } from '../company.service';
+
+@Component({
+  selector: 'app-company',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './company.component.html',
+  styleUrl: './company.component.css'
+})
+export class CompanyComponent {
+
+  company: Company = new Company("", "", "", "");
+
+  constructor(private companyService: CompanyService) { }
+
+  addCompany() {
+    this.companyService.addCompany(this.company).subscribe();
+  }
+}
